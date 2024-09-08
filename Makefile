0fe881dfc6b4ec3b9e4e85c74656e545d4bbf148
deanqx <110404616+deanqx@users.noreply.github.com>
2024-09-08 19:11:58 +0200

    Fixed weird bug at Makefile@--mmcu=atmega328p:11

--- Makefile
+++ Makefile
@@ -1,12 +1,27 @@
 SRC_DIR = src
 BUILD_DIR = build
 
-default:
+OPTIMIZATION = -Os
+
+all: compile upload
+
+compile:
 	@mkdir -p $(BUILD_DIR)
-	avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o $(BUILD_DIR)/compiled.o $(SRC_DIR)/main.c
-	avr-gcc -o $(BUILD_DIR)/compiled.bin $(BUILD_DIR)/compiled.o
+	avr-gcc -c $(OPTIMIZATION) -DF_CPU=16000000UL -mmcu=atmega328p -o $(BUILD_DIR)/compiled.o $(SRC_DIR)/main.c
+	avr-gcc -mmcu=atmega328p -o $(BUILD_DIR)/compiled.bin $(BUILD_DIR)/compiled.o
 	avr-objcopy -O ihex -R .eeprom $(BUILD_DIR)/compiled.bin $(BUILD_DIR)/compiled.hex
-	sudo avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:$(BUILD_DIR)/compiled.hex
+
+upload:
+	avrdude -v -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:$(BUILD_DIR)/compiled.hex
+
+size: compile
+	avr-size --format=avr --mcu=atmega328p $(BUILD_DIR)/compiled.bin
+
+asm:
+	@mkdir -p $(BUILD_DIR)
+	avr-gcc -S -Os -DF_CPU=16000000UL -mmcu=atmega328p -o $(BUILD_DIR)/compiled.asm $(SRC_DIR)/main.c
 
 clean:
 	rm -r $(BUILD_DIR)
+
+.PHONY: upload size clean
--- src/main.c
+++ src/main.c
@@ -1,7 +1,44 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void)
+// #define IN1_PIN PORTB3
+// #define IN2_PIN PORTB2
+// #define IN3_PIN PORTB1
+// #define IN4_PIN PORTB0
+// 
+// typedef struct
+// {
+//     short steps_per_revolution;
+//     float steps_per_degree;
+// } MotorController;
+
+// void init_motor_controller(MotorController* controller, float steps_per_degree)
+// {
+//     controller->steps_per_revolution = (short)(360.0f / steps_per_degree);
+//     controller->steps_per_degree = steps_per_degree;
+// 
+//     DDRB |= 1 << IN1_PIN;
+//     DDRB |= 1 << IN2_PIN;
+//     DDRB &= ~(1 << IN3_PIN);
+//     DDRB &= ~(1 << IN4_PIN);
+// }
+// 
+// void step()
+// {
+//    PORTB |= 1 << IN1_PIN;
+//    _delay_ms(1000);
+//    PORTB &= ~(1 << IN1_PIN);
+//    PORTB |= 1 << IN2_PIN;
+//    _delay_ms(1000);
+//    PORTB &= ~(1 << IN2_PIN);
+// }
+
+void test_abc()
+{
+}
+
+
+void led_test()
 {
     // internal LED
     DDRB |= 1 << DDB5;
@@ -9,8 +46,24 @@ int main(void)
     while (1)
     {
         PORTB |= 1 << PORTB5;
-        _delay_ms(1000);
+        _delay_ms(3000);
         PORTB &= ~(1 << PORTB5);
-        _delay_ms(2000);
+        _delay_ms(1000);
     }
+}
+
+int main(void)
+{
+    led_test();
+    return 0;
+
+    // MotorController controller;
+    // init_motor_controller(&controller, 11.32f);
+
+    // while(1)
+    // {
+    // step();
+    // }
+
+    // return 0;
 }

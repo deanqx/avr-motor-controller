5746081e7b9595a74869fe331333ba0d9e930a5d
deanqx <110404616+deanqx@users.noreply.github.com>
2024-09-10 19:34:25 +0200

    Small motor is working

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <stdint.h>
 #include <util/delay.h>
 
 #define IN1_PIN PORTB0
@@ -6,94 +7,92 @@
 #define IN3_PIN PORTB2
 #define IN4_PIN PORTB3
 
-#define DELAY 500
-
 typedef struct
 {
-    short steps_per_revolution;
-    float steps_per_degree;
+    uint16_t steps_per_revolution;
+    float degree_per_full_step;
+
+    // Has to be set using set_rpm()
+    float rpm;
+    uint16_t delay_between_steps_ms;
 } MotorController;
 
-void init_motor_controller(MotorController* controller, float steps_per_degree)
+void delay_ms(uint16_t ms)
 {
-    controller->steps_per_revolution = (short)(360.0f / steps_per_degree);
-    controller->steps_per_degree = steps_per_degree;
+    while (ms--)
+    {
+        _delay_ms(1);
+    }
 }
 
-void step()
+void mc_set_rpm(MotorController* controller, float rpm)
 {
-    DDRB |= 1 << IN1_PIN;
-    DDRB &= ~(1 << IN3_PIN);
-    PORTB |= 1 << IN1_PIN;
-    _delay_ms(DELAY);
+    controller->rpm = rpm;
+    // 1min=60000ms
+    controller->delay_between_steps_ms = 60000 / (controller->steps_per_revolution * rpm);
+}
+
+// @param degree_per_micro_step Full Step Angle / 4
+void mc_init(MotorController* controller, float degree_per_micro_step)
+{
+    controller->steps_per_revolution = (short)(360.0f / degree_per_micro_step);
+    controller->degree_per_full_step = degree_per_micro_step;
+
+    mc_set_rpm(controller, 1.0f);
+
+    DDRB |= (1 << IN1_PIN);
+    DDRB |= (1 << IN2_PIN);
+    DDRB |= (1 << IN3_PIN);
+    DDRB |= (1 << IN4_PIN);
+}
+
+void mc_step_for_degree(const MotorController* controller, float degree)
+{
+    PORTB |= (1 << IN1_PIN);
+    delay_ms(controller->delay_between_steps_ms);
     PORTB &= ~(1 << IN1_PIN);
 
-    //DDRB |= 1 << IN2_PIN;
-    //DDRB &= ~(1 << IN4_PIN);
-    //PORTB |= 1 << IN2_PIN;
-    //_delay_ms(DELAY);
-    //PORTB &= ~(1 << IN2_PIN);
+    PORTB |= (1 << IN2_PIN);
+    delay_ms(controller->delay_between_steps_ms);
+    PORTB &= ~(1 << IN2_PIN);
 
-    //DDRB |= 1 << IN3_PIN;
-    //DDRB &= ~(1 << IN1_PIN);
-    //PORTB |= 1 << IN3_PIN;
-    //_delay_ms(DELAY);
-    //PORTB &= ~(1 << IN3_PIN);
+    PORTB |= (1 << IN3_PIN);
+    delay_ms(controller->delay_between_steps_ms);
+    PORTB &= ~(1 << IN3_PIN);
 
-    //DDRB |= 1 << IN4_PIN;
-    //DDRB &= ~(1 << IN2_PIN);
-    //PORTB |= 1 << IN4_PIN;
-    //_delay_ms(DELAY);
-    //PORTB &= ~(1 << IN4_PIN);
-}
-
-void step2()
-{
-DDRB |= (1 << IN1_PIN);
-DDRB |= (1 << IN2_PIN);
-DDRB |= (1 << IN3_PIN);
-DDRB |= (1 << IN4_PIN);
-
-PORTB |= (1 << IN1_PIN);
-_delay_ms(DELAY);
-PORTB &= ~(1 << IN1_PIN);
-
-PORTB |= (1 << IN2_PIN);
-_delay_ms(DELAY);
-PORTB &= ~(1 << IN2_PIN);
-
-PORTB |= (1 << IN3_PIN);
-_delay_ms(DELAY);
-PORTB &= ~(1 << IN3_PIN);
-
-PORTB |= (1 << IN4_PIN);
-_delay_ms(DELAY);
-PORTB &= ~(1 << IN4_PIN);
+    PORTB |= (1 << IN4_PIN);
+    delay_ms(controller->delay_between_steps_ms);
+    PORTB &= ~(1 << IN4_PIN);
 }
 
 void led_test()
 {
     // internal LED
-    DDRB |= 1 << DDB5;
+    DDRB |= (1 << DDB5);
 
     while (1)
     {
-        PORTB |= 1 << PORTB5;
-        _delay_ms(3000);
+        PORTB |= (1 << PORTB5);
+        delay_ms(3000);
         PORTB &= ~(1 << PORTB5);
-        _delay_ms(1000);
+        delay_ms(1000);
     }
 }
 
 int main(void)
 {
-    // led_test();
+    //led_test();
 
     MotorController controller;
-    init_motor_controller(&controller, 11.32f);
+    mc_init(&controller, 0.3515625f);
+    mc_set_rpm(&controller, 20.0f);
+    
+    // internal LED
+    DDRB |= 1 << DDB5;
 
-    while(1)
+    while (1)
     {
-        step2();
+        mc_step_for_degree(&controller, 360.0f);
+        PORTB ^= (1 << PORTB5);
     }
 }

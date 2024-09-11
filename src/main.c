06fad045799eded0b67b5f8c98e771fe3fc52a21
deanqx <110404616+deanqx@users.noreply.github.com>
2024-09-11 16:26:26 +0200

    Added buttons to stop rotation

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <avr/io.h>
 #include <util/delay.h>
 
 #include "motor_controller.h"
@@ -5,15 +6,27 @@
 void led_test()
 {
     // internal LED
-    DDRB |= (1 << DDB5);
+    DDRB |= (1 << PB5);
 
     while (1)
     {
-        PORTB |= (1 << PORTB5);
+        PORTB |= (1 << PB5);
         _delay_ms(3000);
-        PORTB &= ~(1 << PORTB5);
+        PORTB &= ~(1 << PB5);
         _delay_ms(1000);
     }
+}
+
+bool switch_top()
+{
+    bool button_pressed = PINC & (1 << PC0);
+    return !button_pressed;
+}
+
+bool switch_buttom()
+{
+    bool button_pressed = PINC & (1 << PC1);
+    return !button_pressed;
 }
 
 int main(void)
@@ -22,16 +35,23 @@ int main(void)
 
     MotorController controller;
     mc_init(&controller, 0.08789f);
-    mc_set_rpm(&controller, 2.5f);
+    mc_set_rpm(&controller, 5.0f);
     mc_calibrate(&controller, 1);
 
     // internal LED
-    DDRB |= 1 << DDB5;
+    DDRB |= 1 << PB5;
+
+    // Top button input
+    DDRC &= ~(1 << PC0);
+    // Buttom button input
+    DDRC &= ~(1 << PC1);
 
     while (1)
     {
         // mc_step_for_degree(&controller, 1, 360.0f);
-        mc_vibrate(&controller);
-        PORTB ^= (1 << PORTB5);
+        mc_step_until(&controller, 1, switch_top);
+        PORTB ^= (1 << PB5);
+        mc_step_until(&controller, -1, switch_buttom);
+        PORTB ^= (1 << PB5);
     }
 }

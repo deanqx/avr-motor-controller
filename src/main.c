bd369556e58094b86f0ecdc2349efedcfa92e19b
deanqx <110404616+deanqx@users.noreply.github.com>
2024-09-27 19:41:51 +0200

    Working on speed controll using poti, LED works

--- src/main.c
+++ src/main.c
@@ -1,62 +1,77 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#include <stdint.h>
 #include <util/delay.h>
 
 #include "motor_controller.h"
+#include "usart.h"
 
-void led_test()
-{
-    // internal LED
-    DDRB |= (1 << PB5);
-
-    while (1)
-    {
-        PORTB |= (1 << PB5);
-        _delay_ms(3000);
-        PORTB &= ~(1 << PB5);
-        _delay_ms(1000);
-    }
-}
+#define TOP_BUTTON_PIN PB5
+#define BUTTOM_BUTTON_PIN PB4
 
 bool switch_top()
 {
-    bool button_pressed = PINC & (1 << PC0);
+    bool button_pressed = PIND & (1 << TOP_BUTTON_PIN);
     return !button_pressed;
 }
 
 bool switch_buttom()
 {
-    bool button_pressed = PINC & (1 << PC1);
+    bool button_pressed = PIND & (1 << BUTTOM_BUTTON_PIN);
     return !button_pressed;
 }
 
 int main(void)
 {
-    // led_test();
-
     cli();
 
-    /*uart_init(BAUD_CALC(115200));  // 8n1 transmission is set as default
+    uart_init(BAUD_CALC(115200));  // 8n1 transmission is set as default
 
     // attach uart stream to stdout & stdin
     stdout = &uart0_io;
-    stdin = &uart0_io;*/
+    stdin = &uart0_io;
 
     MotorController controller;
-    mc_init(&controller, 0.08789f);
+    mc_init(&controller, 0.9f);
     mc_set_rpm(&controller, 0.1f);
-    mc_set_rpm(&controller, 5.0f);
     mc_calibrate(&controller, 1);
 
-    // internal LED
-    DDRB |= 1 << PB5;
+    // internal LED as direction indicator
+    DDRB |= (1 << PB5);
+    DDRD &= ~(1 << TOP_BUTTON_PIN);
+    DDRD &= ~(1 << BUTTOM_BUTTON_PIN);
 
-    // Top button input
-    DDRC &= ~(1 << PC0);
-    // Buttom button input
-    DDRC &= ~(1 << PC1);
+    // Set the ADC prescaler to 128 (16MHz / 128 = 125KHz)
+    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+    // Using AVcc as reference and select ADC0
+    ADMUX |= (1 << REFS0) | ADC0D;
+    // Enable the Analog Digital Converter
+    ADCSRA |= (1 << ADEN);
+
+    DDRD |= (1 << PD6);
+    // Configure Timer0 for Fast PWM mode
+    TCCR0A |= (1 << WGM00) | (1 << WGM01);
+    // Clear OC0A on Compare Match
+    TCCR0A |= (1 << COM0A1);
+    // No prescaling
+    TCCR0B |= (1 << CS00);
 
     sei();
+
+    while (1)
+    {
+        // Start conversion
+        ADCSRA |= (1 << ADSC);
+        // Wait until conversion is complete
+        while (ADCSRA & (1 << ADSC));
+
+        // Map 1023 to 255
+        uint8_t brightness = ADC / (1023 / 255);
+        printf("%d; %d\r\n", ADC, brightness);
+
+        // Set Timer0 duty cycle for PD6
+        OCR0A = brightness;
+    }
 
     while (1)
     {
